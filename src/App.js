d417071524d6df46d4a926b5eadfa7f7cf4dd394
asadullahbappi <58213620+asadullahbappi@users.noreply.github.com>
2020-09-16 14:51:43 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import Pokecard from './pokecard'
+import Pokegame from './pokegame' 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <Pokecard 
-        id= {4} 
-        name= "Charmander"
-        type= "fire"
-        exp={62}
-    />
-    
+ <Pokegame />
     </div>
   );
 }
